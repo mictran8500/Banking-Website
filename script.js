ab1b393cef1db4bbb3194e2e44a8f388e839b764
Michael Tran <mhtran.hy@gmail.com>
2022-01-24 23:23:07 -0800

    added operations tabbing component

--- script.js
+++ script.js
@@ -66,3 +66,26 @@ learnMoreBtn.addEventListener('click', function (e) {
     behavior: 'smooth',
   });
 });
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+// Tabbing Operations Component
+tabsContainer.addEventListener('click', function (e) {
+  const currTab = e.target.closest('.operations__tab');
+
+  // Guard clause: incase a click happens somewhere else on the container and closest results in null
+  if (!currTab) return;
+
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  currTab.classList.add('operations__tab--active');
+
+  // Activate content area
+  tabsContent.forEach(cont =>
+    cont.classList.remove('operations__content--active')
+  );
+  document
+    .querySelector(`.operations__content--${currTab.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
